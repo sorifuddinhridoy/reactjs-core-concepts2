9afaaa4fc458e7ebe1b86162d58c47e47852cb2b
sorifuddinhridoy <121661222+sorifuddinhridoy@users.noreply.github.com>
2023-03-22 06:40:49 +0600

    increase-decrease

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const products =[
-    {name: 'laptop', price: '53000'},
-    {name: 'Phone', price: '93000'},
-    {name: 'watch', price: '33000'},
-    {name: 'tablet', price: '5000'}
-  ]
+
   return (
     <div className="App">
-      {
-        products.map(product => <Product name={product.name} price={product.price}></Product>)
-      }
-     {/* <Product name="laptop" price="7500"></Product>
-     <Product name="phone" price="75000"></Product>
-     <Product name="watch" price="3000"></Product> */}
+      <Counter></Counter>
     </div>
   );
+}
+
+function Counter(){
+  const [count, setCount] = useState(55)
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+     
+    
+  
+  // const increaseCount = () =>{
+  //   const newCount = count + 1;
+  //   setCount(newCount);
+  // }
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
 }
 function Product(props){
   return (
@@ -28,3 +39,4 @@ function Product(props){
   )
 }
 export default App;
+
